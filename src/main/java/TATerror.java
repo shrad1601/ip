dbf3c759e7ce28e95f855827759be21103d3773d
Shraddha Sriram <shrad1601@Shraddhas-MacBook-Air-2.local>
2026-08-27 14:50:10 +0800

    Rename Duke to TATerror, add greet and exit

---
+++ src/main/java/TATerror.java
@@ -0,0 +1,17 @@
+public class TATerror {
+    public static void main(String[] args) {
+        String banner = " _____ _         _____                          \n"
+
+                + "|_   _/ \\       |_   _|__ _ __ _ __ ___  _ __   \n"
+                + "  | |/ _ \\        | |/ _ \\ '__| '__/ _ \\| '__|  \n"
+                + "  | / ___ \\       | |  __/ |  | | | (_) | |     \n"
+                + "  |_/_/   \\_\\     |_|\\___|_|  |_|  \\___/|_|     \n";
+        System.out.println(banner);
+        System.out.println("____________________________________________________________");
+        System.out.println("Oh. It's you.");
+        System.out.println("I'm TA Terror. Try not to waste my time.");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Try to disappoint someone else next time.");
+        System.out.println("____________________________________________________________");
+    }
+}
