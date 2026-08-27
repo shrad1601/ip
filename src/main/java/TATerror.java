c45a8d06e445b1c5468b316b0588f06ee5cf7564
shrad1601 <shrad0116@gmail.com>
2026-08-27 16:20:27 +0800

    Add todo, deadline, and event task types

--- src/main/java/TATerror.java
+++ src/main/java/TATerror.java
@@ -15,6 +15,8 @@ public class TATerror {
 
         String[] tasks = new String[100];
         boolean[] isDone = new boolean[100];
+        String[] types = new String[100];
+        String[] extraInfo = new String[100];
         int taskCount = 0;
 
         Scanner scanner = new Scanner(System.in);
@@ -24,23 +26,42 @@ public class TATerror {
             System.out.println("____________________________________________________________");
             if (input.equals("list")) {
                 for (int i = 0; i < taskCount; i++) {
-                    String status = isDone[i] ? "[X]" : "[ ]";
-                    System.out.println((i + 1) + "." + status + " " + tasks[i]);
+                    System.out.println((i + 1) + "." + formatTask(i, tasks, isDone, types, extraInfo));
                 }
             } else if (input.startsWith("mark ")) {
                 int index = Integer.parseInt(input.substring(5)) - 1;
                 isDone[index] = true;
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  [X] " + tasks[index]);
+                System.out.println("  " + formatTask(index, tasks, isDone, types, extraInfo));
             } else if (input.startsWith("unmark ")) {
                 int index = Integer.parseInt(input.substring(7)) - 1;
                 isDone[index] = false;
                 System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("  [ ] " + tasks[index]);
-            } else {
-                tasks[taskCount] = input;
+                System.out.println("  " + formatTask(index, tasks, isDone, types, extraInfo));
+            } else if (input.startsWith("todo ")) {
+                types[taskCount] = "T";
+                tasks[taskCount] = input.substring(5);
+                extraInfo[taskCount] = "";
+                addTask(tasks, isDone, types, extraInfo, taskCount);
                 taskCount++;
-                System.out.println("added: " + input);
+            } else if (input.startsWith("deadline ")) {
+                String rest = input.substring(9);
+                String[] parts = rest.split(" /by ");
+                types[taskCount] = "D";
+                tasks[taskCount] = parts[0];
+                extraInfo[taskCount] = "by: " + parts[1];
+                addTask(tasks, isDone, types, extraInfo, taskCount);
+                taskCount++;
+            } else if (input.startsWith("event ")) {
+                String rest = input.substring(6);
+                String[] fromSplit = rest.split(" /from ");
+                String description = fromSplit[0];
+                String[] toSplit = fromSplit[1].split(" /to ");
+                types[taskCount] = "E";
+                tasks[taskCount] = description;
+                extraInfo[taskCount] = "from: " + toSplit[0] + " to: " + toSplit[1];
+                addTask(tasks, isDone, types, extraInfo, taskCount);
+                taskCount++;
             }
             System.out.println("____________________________________________________________");
             input = scanner.nextLine();
@@ -50,5 +71,19 @@ public class TATerror {
         System.out.println("Bye. Try to disappoint someone else next time.");
         System.out.println("____________________________________________________________");
         scanner.close();
+    }
+
+    private static void addTask(String[] tasks, boolean[] isDone, String[] types,
+                                String[] extraInfo, int index) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + formatTask(index, tasks, isDone, types, extraInfo));
+        System.out.println("Now you have " + (index + 1) + " tasks in the list.");
+    }
+
+    private static String formatTask(int index, String[] tasks, boolean[] isDone,
+                                     String[] types, String[] extraInfo) {
+        String status = isDone[index] ? "[X]" : "[ ]";
+        String extra = extraInfo[index].isEmpty() ? "" : " (" + extraInfo[index] + ")";
+        return "[" + types[index] + "]" + status + " " + tasks[index] + extra;
     }
 }
