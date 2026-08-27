f755b71f2f4eff6016d8c4899c6b5e8bd5ba9352
shrad1601 <shrad0116@gmail.com>
2026-08-27 16:12:33 +0800

    Add echo functionality with exit on bye

--- src/main/java/TATerror.java
+++ src/main/java/TATerror.java
@@ -1,7 +1,8 @@
+import java.util.Scanner;
+
 public class TATerror {
     public static void main(String[] args) {
         String banner = " _____ _         _____                          \n"
-
                 + "|_   _/ \\       |_   _|__ _ __ _ __ ___  _ __   \n"
                 + "  | |/ _ \\        | |/ _ \\ '__| '__/ _ \\| '__|  \n"
                 + "  | / ___ \\       | |  __/ |  | | | (_) | |     \n"
@@ -11,7 +12,20 @@ public class TATerror {
         System.out.println("Oh. It's you.");
         System.out.println("I'm TA Terror. Try not to waste my time.");
         System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+            input = scanner.nextLine();
+        }
+
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Try to disappoint someone else next time.");
         System.out.println("____________________________________________________________");
+        scanner.close();
     }
 }
